d38185ad1f0943bf751e662546f06df3d55bd769
Fabio Teixeira <fabio1079@gmail.com>
2013-10-12 15:18:16 -0300

    Add links to community members block
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- theme.js
+++ theme.js
@@ -1,27 +1,21 @@
 (function($) {
-  jQuery(".box-2 .link-list-block .block-title").click(function(){
-    jQuery(this).parent().children("ul").stop().slideToggle();
+  // Toggle left menu
+  $(".box-2 .link-list-block .block-title").click(function(){
+    $(this).parent().children("ul").stop().slideToggle();
   });
 
-  jQuery(document).ready(function(){
-    /*http://localhost:3000/account/user_data*/
-    var community = window.location.href.split("/")[3];
+  // Add links to community members block
+  if(jQuery(".members-block .block-footer-content").length == 1) {
+    (function($){
+      $(".profile-info-options").bind("DOMSubtreeModified",function(){
+        var link_enter = $(".profile-info-options a.icon-add");
+        var link_leave = $(".profile-info-options a.leave-community");
+        var link_join  = $(".profile-info-options a.join-community");
 
-    var enter = "<a href='/account/login' class='thickbox' id='link_login'>"+
-                "<i class='icon-menu-login'></i><strong>Entrar</strong></a>";
-
-    var leave = "<a href='/profile/"+community+"/leave' "+
-                "class='button icon-delete with-text leave-community' "+
-                "style='position: relative;' title='Leave community'>"+
-                "&nbsp;<span><span>Leave community</span></span></a>";
-
-    var join = "<a href='/profile/"+community+"/join'"+
-                "class='button icon-add with-text join-community'"+
-                "style='position: relative;' title='Join community'>"+
-                "&nbsp;<span><span>Join</span></span></a>";
-
-
-
-    jQuery(".members-block .block-footer-content").append(enter);
-  });
+        $(".members-block .block-footer-content").append(link_enter);
+        $(".members-block .block-footer-content").append(link_join);
+        $(".members-block .block-footer-content").append(link_leave);
+      });
+    })(jQuery);
+  }
 })(jQuery);
